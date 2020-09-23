cff7a5aef53229066a72ee6aec2678e79c273f4e
Memorik <70646403+Memorik@users.noreply.github.com>
2020-09-24 01:42:49 +0300

    Add files via upload
    Create Main

---
+++ normar_calc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Normar_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chose;
+            while (true) 
+            {
+
+                Console.Write("Enter expression: "); 
+                Console.WriteLine(Calculator.Calculate(Console.ReadLine()));
+                Console.WriteLine("If u want to exit write: '-+-' ");
+                Console.WriteLine("If u want to calculate: Enter");
+                chose = Console.ReadLine();
+                if (chose == "-+-")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
